379dd455022c49f834118e36f61d63c271d49512
shfana <wambitafana@gmail.com>
2024-05-16 16:15:55 +0300

    Correct the function to display first parameter

--- displayfirstparam.go
+++ displayfirstparam.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 // Write a program that displays its first argument, if there is one.
+// too easy to be the solution
 func Display() {
 	if len(os.Args) > 1 {
 		fmt.Println(os.Args[1])
 	}
 }
 
-func mainc() {
-	Display()
+func mainm() {
+	if len(os.Args) > 1 {
+		firstparam := os.Args[1]
+		for _, c := range firstparam {
+			z01.PrintRune(c)
+		}
+		z01.PrintRune('\n')
+	} else {
+		return
+	}
 }

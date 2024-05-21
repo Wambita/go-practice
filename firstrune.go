4eb37460291070456fa5be5a801ae95240a6ffd4
shfana <wambitafana@gmail.com>
2024-05-22 00:23:44 +0300

    Add first rune function

---
+++ firstrune.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/01-edu/z01"
+
+// Write a function that returns the first rune of a string.
+func main() {
+	z01.PrintRune(FirstRune(" Hello!"))
+	z01.PrintRune(FirstRune("Salut!"))
+	z01.PrintRune(FirstRune("Ola!"))
+	z01.PrintRune('\n')
+
+	z01.PrintRune(firstRune(" Hello!"))
+	z01.PrintRune(firstRune("Salut!"))
+	z01.PrintRune(firstRune("Ola!"))
+	z01.PrintRune('\n')
+}
+
+func FirstRune(s string) rune {
+	srune := []rune(s)
+
+	for _, char := range srune {
+		if char != ' ' {
+			return char
+		}
+	}
+	// if string contains only space , return a default rune
+	return ' '
+}
+
+// attempt 2 easy
+func firstRune(s string) rune {
+	srune := []rune(s)
+
+	return srune[0]
+}

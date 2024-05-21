51900f435cf087dd7dad5e4b9434e84effacf020
shfana <wambitafana@gmail.com>
2024-05-21 23:34:54 +0300

    Add lastword function

--- lastword.go
+++ lastword.go
@@ -11,27 +11,33 @@ import (
 // The output will be followed by a newline ('\n').
 // If the number of arguments is different from 1, or if there are no word, the program displays nothing.
 func main() {
-	if len(os.Args) == 2 {
-		var word string
-		input := os.Args[1]
-		if input == " " {
-			return
-		}
-		sentence := []byte(input)
-		for i := len(sentence) - 1; i >= 0; i-- {
-			if sentence[i] == ' ' {
-				break
-			} else {
-				word += string(sentence[i])
-			}
-		}
-		words := []rune(word)
-		for i := len(words) - 1; i >= 0; i-- {
-			z01.PrintRune(words[i])
-		}
-		z01.PrintRune('\n')
-
-	} else {
+	if len(os.Args) != 2 {
 		return
 	}
+	input := os.Args[1]
+	if input == " " {
+		return
+	}
+
+	sentence := []rune(input)
+	var word string
+	var lastWord string
+
+	// iterate over input string in reverse to find last word
+	for i := len(sentence) - 1; i >= 0; i-- {
+		// if space is encountered and word isn't empty == found last word
+		if sentence[i] == ' ' {
+			if len(word) > 0 {
+				lastWord = word
+				break
+			}
+		} else {
+			word = string(sentence[i]) + word // to print correctly
+			// if you use word += sentence[i] // have to print in reverse
+		}
+	}
+	for _, char := range lastWord {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
 }

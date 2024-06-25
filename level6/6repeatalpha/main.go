434701c212964345a6f63cde60260f078f6621b0
shfana <wambitafana@gmail.com>
2024-06-25 11:12:06 +0300

    Feat: add program for repeat alpha

--- level6/6repeatalpha/main.go
+++ level6/6repeatalpha/main.go
@@ -7,31 +7,37 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 4 {
+	if len(os.Args) != 2 {
 		return
 	}
 	word := os.Args[1]
-	LetterToBeReplaced := os.Args[2]
-	LetterToBeReplacedWith := os.Args[3]
-	var finalString string
 
 	for _, char := range word {
-		for _, c := range LetterToBeReplaced {
-			for _, v := range LetterToBeReplacedWith {
-				if char == c {
-					finalString += string(v)
-				} else {
-					finalString += string(char)
+		if char >= 'a' && char <= 'z' {
+			count := 0
+			for i := 'a'; i <= 'z'; i++ {
+				count++
+				if char == i {
+					for j := 0; j < count; j++ {
+						z01.PrintRune(char)
+					}
+					break
 				}
 			}
+		} else if char >= 'A' && char <= 'Z' {
+			count := 0
+			for i := 'A'; i <= 'Z'; i++ {
+				count++
+				if char == i {
+					for j := 0; j < count; j++ {
+						z01.PrintRune(char)
+					}
+					break
+				}
+			}
+		} else {
+			z01.PrintRune(char)
 		}
-		PrintString(finalString)
-	}
-}
-
-func PrintString(word string) {
-	for _, char := range word {
-		z01.PrintRune(char)
 	}
 	z01.PrintRune('\n')
 }

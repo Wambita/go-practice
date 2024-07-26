26f64fbab3d4dcb37e2155c09f1818dcabfdaca9
Wambita <wambitafana@gmail.com>
2024-07-26 14:46:26 +0300

    Fix: slice function

--- check/slice/main.go
+++ check/slice/main.go
@@ -1,54 +1,86 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/chars"
+	"github.com/01-edu/go-tests/lib/random"
+	"github.com/01-edu/go-tests/solutions"
 )
 
 func main() {
-	a := []string{"coding", "algorithm", "ascii", "package", "golang"}
-	fmt.Printf("%#v\n", Slice(a, 1))
-	fmt.Printf("%#v\n", Slice(a, 2, 4))
-	fmt.Printf("%#v\n", Slice(a, -3))
-	fmt.Printf("%#v\n", Slice(a, -2, -1))
-	fmt.Printf("%#v\n", Slice(a, 2, 0))
+	elems := [][]interface{}{
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-3,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 4,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			-2, -1,
+		},
+		{
+			[]string{"coding", "algorithm", "ascii", "package", "golang"},
+			2, 0,
+		},
+	}
+
+	s := random.StrSlice(chars.Words)
+
+	elems = append(elems, []interface{}{s, -len(s) - 10, -len(s) - 5})
+
+	for i := 0; i < 3; i++ {
+		s = random.StrSlice(chars.Words)
+		elems = append(elems, []interface{}{s, random.IntBetween(-len(s)-10, len(s)+10), random.IntBetween(-len(s)-8, len(s)+10)})
+	}
+
+	for _, a := range elems {
+		challenge.Function("Slice", Slice, solutions.Slice, a...)
+	}
 }
 
 func Slice(a []string, nbrs ...int) []string {
-	var slice = []string{}
-	//length of strings is one
-	//negative num
-	if len(nbrs) == 1 {
-		if nbrs[0] < 1 {
-			nbrs[0] = -nbrs[0]
-			pos := len(a) - nbrs[0]
-			a = a[pos:]
-			return a
-		} else {
-			a = a[nbrs[0]:]
-			return a
-		}
+	length := len(a)
 
-	}
-
-	if len(nbrs) > 1 {
-		start := nbrs[0]
-		finish := nbrs[len(nbrs)-1]
-
-		if start == 0 || finish == 0 {
-			return nil
-		}
-
-		if start < 0 {
-			starts := -start
-			start = len(a) - starts
-		}
-		if finish < 0 {
-			finishes := -finish
-			finish = len(a) - finishes
-		}
-		a = a[start:finish]
+	if len(nbrs) == 0 {
 		return a
 	}
 
-	return slice
+	start := nbrs[0]
+	end := length
+
+	if len(nbrs) > 1 {
+		end = nbrs[1]
+	}
+
+	// Handle negative indices
+	if start < 0 {
+		start = length + start
+	}
+	if end < 0 {
+		end = length + end
+	}
+
+	// Ensure start and end are within bounds
+	if start < 0 {
+		 start = 0
+		return []string{}
+	}
+	if end > length {
+		// end = length
+		return []string{}
+	}
+
+	// Return nil if start is greater than end or length
+	if start >= length || start >= end {
+		return nil
+	}
+
+	return a[start:end]
 }

d4bac0115d0a1058cfb45ae1e5126a8c3d7c843d
shfana <wambitafana@gmail.com>
2024-08-18 01:39:35 +0300

    Feat: add for each function

---
+++ check/foreach/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/lib/random"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func add0(i int) {
+	fmt.Println(i)
+}
+
+func add1(i int) {
+	fmt.Println(i + 1)
+}
+
+func add2(i int) {
+	fmt.Println(i + 2)
+}
+
+func add3(i int) {
+	fmt.Println(i + 3)
+}
+
+func main() {
+	functions := []func(int){add0, add1, add2, add3}
+
+	type node struct {
+		f func(int)
+		a []int
+	}
+
+	table := []node{{
+		f: add0,
+		a: []int{1, 2, 3, 4, 5, 6},
+	}}
+
+	// 15 random slice of random ints with a random function from selection
+	for i := 0; i < 15; i++ {
+		function := functions[random.IntBetween(0, len(functions)-1)]
+		table = append(table, node{
+			f: function,
+			a: random.IntSliceBetween(-1000000, 1000000),
+		})
+	}
+
+	for _, arg := range table {
+		challenge.Function("ForEach", ForEach, solutions.ForEach, arg.f, arg.a)
+	}
+}
+
+func ForEach(f func(int), a []int) {
+	if len(a) == 0 {
+		return
+	}
+	for _, n := range a {
+		f(n)
+	}
+}
